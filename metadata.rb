2331ba1d99ad8d8abeff631fe97d95e02cb08c68
Julian C. Dunn <jdunn@opscode.com>
2013-03-29 17:02:08 -0400

    Added recipe for user creation on the targets.

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,8 @@ name             'tfchefint'
 maintainer       'Opscode, Inc.'
 maintainer_email 'jdunn@opscode.com'
 license          'Apache 2.0'
-description      'Installs/Configures tfchefint'
+description      'Installs/Configures TeamForge and Chef integration'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'users'
