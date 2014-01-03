6e61802258aff98b8d59ad3dba4989d77db94b03
Noorul Islam K M <noorul@collab.net>
2014-01-03 10:48:24 +0530

    Update meta data
    * .gitignore: Update
    
    * .gitreview: Git review configuration.
    
    * Gemfile: Pin gems
    
    * Vagrantfile: Update
    
    * chefignore: Add cookbook tar ball
    
    * metadata.rb: Update
    
    Change-Id: Ie3a0a231fb2d918858a635fc207faea63951fa40

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'berkshelf'
+gem 'berkshelf', '~> 2.0.10'
+gem 'rspec', :require => 'spec'
+gem "chefspec", "~> 3.0.1"
 gem 'thor-foodcritic'
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             'tfchefint'
-maintainer       'Opscode, Inc.'
-maintainer_email 'jdunn@opscode.com'
+name             'ctfchefint'
+maintainer       'CollabNet, Inc.'
+maintainer_email 'dev-devops@collab.net'
 license          'Apache 2.0'
 description      'Installs/Configures TeamForge and Chef integration'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
